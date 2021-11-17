71f926197bd82f820998a216cf3ba8fc3f781b89
SumanthShettigar <93831172+SumanthShettigar@users.noreply.github.com>
2021-11-17 19:30:56 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -2,8 +2,8 @@ tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListColle
 aria2c --enable-rpc --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --bt-max-peers=0 \
    --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=0 --bt-tracker="[$tracker_list]"\
-   --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=2 --bt-tracker="[$tracker_list]"\
+   --max-overall-upload-limit=1K --max-concurrent-downloads=10 --continue=true \
    --peer-id-prefix=-qB4380- --user-agent=qBittorrent/4.3.8 --peer-agent=qBittorrent/4.3.8 \
    --disk-cache=32M --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
    --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
